116fbd38c2379036b6f7da036cbf4b03e167e407
Patrick Campos <pcampos.desenvolvimento@gmail.com>
2026-02-04 15:52:58 -0300

    chore: self-hosted fonts with next font

--- source/frontend/src/pages/_app.tsx
+++ source/frontend/src/pages/_app.tsx
@@ -1,18 +1,26 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
+import { Fraunces, Work_Sans } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/styles/globals.css";
 
 const queryClient = new QueryClient();
+const displayFont = Fraunces({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  variable: "--font-display",
+});
+const bodyFont = Work_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-body",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      </Head>
-      <Component {...pageProps} />
+      <div className={`${displayFont.variable} ${bodyFont.variable}`}>
+        <Component {...pageProps} />
+      </div>
     </QueryClientProvider>
   );
 }
